8e1325f98619f6070a1b7c067c1bc22cac4b98dd
Joanne <90553540+joannemas@users.noreply.github.com>
2024-06-05 16:19:23 +0200

    :sparkles: Add rooms

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,23 @@ import http from 'http';
 import ip from 'ip';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import crypto from 'crypto';
+
 const app = express();
 const server = http.createServer(app);
 const PORT = 3000;
 const io = new Server(server, {
     cors: {
         origin: '*',
-        }
-})
-app.use(cors())
-app.get('/', (req, res) => {
-    res.json('ip address: http://' + ip.address()+':'+PORT);    
+    }
 });
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.json('ip address: http://' + ip.address() + ':' + PORT);
+});
+
+const rooms = {};
 
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -23,29 +28,50 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
         socket.broadcast.emit('user disconnected');
     });
+
     socket.on('message', (msg) => {
         console.log('message: ' + msg);
         io.emit('message', msg);
     });
-    
-    socket.on('room', (room, msg) => {
-        console.log('room: ' + room + ' message: ' + msg);
-        io.to(room).emit('message', msg);
+
+    socket.on('create room', (username) => {
+        const roomCode = crypto.randomBytes(3).toString('hex');
+        if (!rooms[roomCode]) {
+            rooms[roomCode] = { users: [], leader: socket.id };
+            rooms[roomCode].users.push({ id: socket.id, name: username });
+            socket.join(roomCode);
+            socket.emit('room created', roomCode);
+            io.to(roomCode).emit('room users', rooms[roomCode].users);
+        } else {
+            socket.emit('room full');
+        }
     });
 
-    socket.on('join', (room) => {
-        console.log('join room: ' + room);
-        socket.join(room);
-        io.to(room).emit('join', room);
+    socket.on('join room', (roomCode, username) => {
+        if (rooms[roomCode] && rooms[roomCode].users.length < 5) {
+            rooms[roomCode].users.push({ id: socket.id, name: username });
+            socket.join(roomCode);
+            io.to(roomCode).emit('room users', rooms[roomCode].users);
+        } else {
+            socket.emit('room full');
+        }
     });
-    socket.on('leave', (room) => {
-        console.log('leave room: ' + room);
-        socket.leave(room);
-        io.to(room).emit('leave', room);
-    });
-})
 
+    socket.on('leave room', (roomCode) => {
+        if (rooms[roomCode]) {
+            rooms[roomCode].users = rooms[roomCode].users.filter(user => user.id !== socket.id);
+            socket.leave(roomCode);
+            io.to(roomCode).emit('room users', rooms[roomCode].users);
+        }
+    });
+
+    socket.on('room message', (roomCode, message) => {
+        if (rooms[roomCode]) {
+            io.to(roomCode).emit('message', message);
+        }
+    });
+});
 
 server.listen(PORT, () => {
-    console.log('Server ip : http://' +ip.address() +":" + PORT);
-})
+    console.log('Server ip : http://' + ip.address() + ":" + PORT);
+});
--- front/script.js
+++ front/script.js
@@ -1,26 +1,69 @@
-let room = '';
-let socketid = '';
-const roomArea = document.querySelector('#room');
-const messageArea = document.querySelector('#message');
 const socket = io('http://localhost:3000');
-socket.on('connect', () => {
-    console.log('Connected');
-});
-socket.on('message', (data) => {
-    console.log(data);
-    document.querySelector('.data').innerText = data;
-});
-socket.on('disconnect', () => {
-    console.log('Disconnected');
-});
+    let roomCode = '';
+    let username = '';
 
-let send = () => {
-    console.log(messageArea.value);
-    //socket.emit('message', `message : ${messageArea.value}`);
-    socket.emit('room', roomArea.value, `Message room ${roomArea.value} : ${messageArea.value}`);
-}
-roomArea.addEventListener('change', (e) => {
-    socket.emit('leave', room);
-    socket.emit('join', e.target.value);
-    room = e.target.value;
-});
+    socket.on('connect', () => {
+        console.log('Connected');
+    });
+
+    socket.on('message', (data) => {
+        const messageDiv = document.createElement('div');
+        messageDiv.textContent = data;
+        document.getElementById('messages').appendChild(messageDiv);
+    });
+
+    socket.on('room users', (users) => {
+        const userList = document.getElementById('users');
+        userList.innerHTML = '';
+        users.forEach(user => {
+            const userItem = document.createElement('li');
+            userItem.textContent = user.name;
+            userList.appendChild(userItem);
+        });
+    });
+
+    socket.on('room full', () => {
+        alert('Room is full. Please try another room.');
+    });
+
+    socket.on('room created', (code) => {
+        roomCode = code;
+        document.getElementById('room-code').textContent = roomCode;
+        document.getElementById('room-code-display').style.display = 'block';
+    });
+
+    function createRoom() {
+        username = document.getElementById('username').value;
+        if (username) {
+            socket.emit('create room', username);
+            document.getElementById('create-room').style.display = 'none';
+            document.getElementById('join-room').style.display = 'none';
+            document.getElementById('chat-room').style.display = 'block';
+        } else {
+            alert('Please enter a username.');
+        }
+    }
+
+    function joinRoomWithCode() {
+        const joinRoomCode = document.getElementById('join-room-code').value;
+        const joinUsername = document.getElementById('join-username').value;
+        if (joinRoomCode && joinUsername) {
+            roomCode = joinRoomCode;
+            username = joinUsername;
+            socket.emit('join room', joinRoomCode, joinUsername);
+            document.getElementById('create-room').style.display = 'none';
+            document.getElementById('join-room').style.display = 'none';
+            document.getElementById('chat-room').style.display = 'block';
+            document.getElementById('room-code').textContent = roomCode;
+        } else {
+            alert('Veuillez compléter tous les champs.');
+        }
+    }
+
+    function sendMessage() {
+        const messageInput = document.getElementById('message-input');
+        if (messageInput.value) {
+            socket.emit('room message', roomCode, `${username}: ${messageInput.value}`);
+            messageInput.value = '';
+        }
+    }
